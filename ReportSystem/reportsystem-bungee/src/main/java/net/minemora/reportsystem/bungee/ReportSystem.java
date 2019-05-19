c12dae2ff968f4a2ecfa9c8b6083e010a54c4bba
BuildTools <unconfigured@null.spigotmc.org>
2019-05-19 18:59:08 -0300

    First Commit

---
+++ ReportSystem/reportsystem-bungee/src/main/java/net/minemora/reportsystem/bungee/ReportSystem.java
@@ -0,0 +1,12 @@
+package net.minemora.reportsystem.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ReportSystem extends Plugin {
+	
+	@Override
+    public void onEnable() {
+		getLogger().info("bungee version loaded!");
+	}
+
+}
---
+++ ReportSystem/reportsystem-spigot/src/main/java/net/minemora/reportsystem/spigot/ReportSystem.java
@@ -0,0 +1,12 @@
+package net.minemora.reportsystem.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ReportSystem extends JavaPlugin {
+	
+	@Override
+    public void onEnable() {
+		getLogger().info("spigot version loaded!");
+	}
+
+}
